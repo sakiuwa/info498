e7ab8f1134e0d0eceebeeffb4df3e64778e4b7f4
Landon Young <young518@uw.edu>
2017-02-27 21:55:31 -0800

    Added Bao's data files + started ui for second question

---
+++ Tab 2/ui.R
@@ -0,0 +1,6 @@
+### UI of second tab
+### by Landon Young
+### 
+### This tab of the shiny app will be looking at correlations between 
+### marijuana users' self-reported mental health and general health 
+
