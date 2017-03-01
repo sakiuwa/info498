84be694683b7decd06c2397386b14e9dfa3025ca
Landon Young <young518@uw.edu>
2017-03-01 15:08:48 -0800

    Added weights.csv

--- Tab 2/ui.R
+++ Tab 2/ui.R
@@ -4,3 +4,7 @@
 ### This tab of the shiny app will be looking at correlations between 
 ### marijuana users' self-reported mental health and general health 
 
+library(ggplot2)
+library(dplyr)
+
+health <- read.csv("./data/health.csv")
