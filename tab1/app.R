352e4002f56af61eaf5ebe6804894c7926326b19
Iris Sun <iriscsun@gmail.com>
2017-03-03 18:46:58 -0800

    updated csv file values to text instead of numbers

--- tab1/app.R
+++ tab1/app.R
@@ -14,6 +14,24 @@ library(ggplot2)
 data <- read.csv("../data/tab1-data.csv")
 data$X <- NULL
 
+data$NEWRACE2[data$NEWRACE2 == 1] <- "White"
+data$NEWRACE2[data$NEWRACE2 == 2] <- "Black/Afr Am"
+data$NEWRACE2[data$NEWRACE2 == 3] <- "Native Am/AK Native "
+data$NEWRACE2[data$NEWRACE2 == 4] <- "Native HI/Other Pac Isl"
+data$NEWRACE2[data$NEWRACE2 == 5] <- "Asian"
+data$NEWRACE2[data$NEWRACE2 == 6] <- "More than one race"
+data$NEWRACE2[data$NEWRACE2 == 7] <- "Hispanic"
+
+data$IRSEX[data$IRSEX == 1] <- "Male"
+data$IRSEX[data$IRSEX == 2] <- "Female"
+
+data$CATAG6[data$CATAG6 == 1] <- "12-17 Years Old"
+data$CATAG6[data$CATAG6 == 2] <- "18-25 Years Old"
+data$CATAG6[data$CATAG6 == 3] <- "26-34 Years Old"
+data$CATAG6[data$CATAG6 == 4] <- "35-49 Years Old"
+data$CATAG6[data$CATAG6 == 5] <- "50-64 Years Old"
+data$CATAG6[data$CATAG6 == 6] <- "65 or Older"
+
 data.user <- data %>% filter(MJEVER == 1)
 data.nonuser <- data %>% filter(MJEVER == 2)
 
