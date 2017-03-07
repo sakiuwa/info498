d3e480c99bea2e8659cc664e79f01c238cda1cd2
Landon Young <young518@uw.edu>
2017-03-07 00:16:33 -0800

    Switched graphs to plotly, close issue #7

--- Tab 2 (Landon)/server.r
+++ Tab 2 (Landon)/server.r
@@ -30,116 +30,52 @@ demo$CATAG6 <- factor(demo$CATAG6,
 users.only <- filter(mjdata, MJEVER == "(1) Yes") %>% select(CASEID, MJAGE, MJREC, MJDAY30A)
 users.only <- left_join(users.only, demo, by = "CASEID")
 
-# Make groups and percentage columns
-grouping <- group_by(users.only, NEWRACE2) %>% summarise(count = n(), nationalcount = sum(ANALWT_C)) %>% mutate(perc = count/sum(count), natperc = nationalcount/sum(nationalcount))
-
-
-
-
-
-# # Make a survey design
-# mjdesign <- svydesign(id=~1, strata=~VESTR, weights=~ANALWT_C , data=users.only)
-# 
-# # Make proportions columns
-# mjage2 <- data.frame(prop.table(svytable(~MJAGE, design=mjdesign)))
-# mjrec2 <- data.frame(prop.table(svytable(~MJREC, design=mjdesign)))
-# mjday30a2 <- data.frame(prop.table(svytable(~MJDAY30A, design=mjdesign)))
-# 
-# users.only$MJAGE2 <- factor(users.only$MJAGE,
-#                              levels = c(1:50, 52:56, 59:62, 64:65, 68:70, 72, 77),
-#                              labels = mjage2$Freq)
-# 
-# users.only$MJREC2 <- factor(users.only$MJREC,
-#                             levels = c("(01) Within the past 30 days", 
-#                                        "(02) More than 30 days ago but within the past 12 mos",
-#                                        "(03) More than 12 months ago",                      
-#                                        "(08) Used at some point in the past 12 mos LOG ASSN",  
-#                                        "(09) Used at some point in the lifetime LOG ASSN",     
-#                                        "(11) Used in the past 30 days LOGICALLY ASSIGNED"),
-#                             labels = mjrec2$Freq)
-# 
-# users.only$MJDAY30A2 <- factor(users.only$MJDAY30A,
-#                             levels = c(1:30),
-#                             labels = mjday30a2$Freq)
-
-
 shinyServer(function(input, output) {
-  
-  # # Reactive function for demographics within MJAGE question
-  # mjagedems <- reactive({
-  #   if(input$demo == 'factor(CATAG6)') {
-  #     temp <- group_by(users.only, MJAGE, CATAG6) %>%  summarise(count = n())
-  #     return()
-  #   } else if (input$demo == 'IRSEX') {
-  #     temp <- group_by(users.only, MJAGE, IRSEX) %>%  summarise(count = n())
-  #     return()
-  #   } else {
-  #     temp <- group_by(users.only, MJAGE, NEWRACE2) %>%  summarise(count = n())
-  #     return(temp)
-  #   }
-  # }) 
-  # 
-  # # Reactive function for demographics within MJREC question
-  # mjrecdems <- reactive({
-  #   if(input$demo == 'factor(CATAG6)') {
-  #     temp <- group_by(users.only, MJREC, CATAG6) %>%  summarise(count = n())
-  #     return()
-  #   } else if (input$demo == 'IRSEX') {
-  #     temp <- group_by(users.only, MJREC, IRSEX) %>%  summarise(count = n())
-  #     return()
-  #   } else {
-  #     temp <- group_by(users.only, MJREC, NEWRACE2) %>%  summarise(count = n())
-  #     return(temp)
-  #   }
-  # }) 
-  # 
-  # # Reactive function for demographics withing MJDAY30A question
-  # mjday30adems <- reactive({
-  #   if(input$demo == 'factor(CATAG6)') {
-  #     temp <- group_by(users.only, MJDAY30A, CATAG6) %>%  summarise(count = n())
-  #     return()
-  #   } else if (input$demo == 'IRSEX') {
-  #     temp <- group_by(users.only, MJDAY30A, IRSEX) %>%  summarise(count = n())
-  #     return()
-  #   } else {
-  #     temp <- group_by(users.only, MJDAY30A, NEWRACE2) %>%  summarise(count = n())
-  #     return(temp)
-  #   }
-  # }) 
-  
   # Reactive function to choose particular graph depending on the selected question.
   plot <- reactive({
-    mjdemo <- input$demo
-    
-    if(input$question == 'MJAGE') {  
-    #   slidevec <- input$agerange
-    #   slidemin <- slidevec[1]
-    #   slidemax <- slidevec[2]
-    #   if(input$demo == 'factor(CATAG6)') {
-    #     mjdemo <- 'factor(CATAG6)'
-    #   } else if (input$demo == 'IRSEX') {
-    #     mjdemo <- 'IRSEX'
-    #   } else {
-    #     mjdemo <- 'factor(NEWRACE2)'
-    #   }
-      ranged.users.only <- filter(users.only, MJAGE >= slidemin, MJAGE <= slidemax) %>% group_by_('MJAGE', mjdemo) %>% summarise(count = n(), nationalcount = sum(ANALWT_C)) %>% mutate(perc = count/sum(count), natperc = nationalcount/sum(nationalcount))
-      return(plot_ly(ranged.users.only, x=~MJAGE, y=~count, color=~factor(CATAG6), type="bar") %>% layout(barmode="stack"))
-    } else if (input$question == 'MJREC') {
-      return(ggplot(users.only, aes_string(x='MJREC', fill=input$demo)) + geom_bar(width = 0.4, colour = "black") + theme(axis.text.y = element_text(size = 8)) + coord_flip())
-    } else {  
-      return(ggplot(users.only, aes_string(x='MJDAY30A', fill=input$demo)) + geom_bar(width=0.4, colour="black") + scale_fill_manual(values=c("#CC6666", "#9999CC", "#66CC99", "#339900", "#66CC00", "#33CC33", "#99CC00"))) # + facet_wrap(~ input$demo))??
+    if(input$question == "MJAGE") {
+      slidevec <- input$agerange
+      slidemin <- slidevec[1]
+      slidemax <- slidevec[2]
+      slider.data <- filter(users.only, MJAGE >= slidemin, MJAGE <= slidemax)
+      if(input$demo == "CATAG6") {
+        data <- group_by(slider.data, MJAGE, CATAG6) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
+        return(plot_ly(data, x=~MJAGE, y=~perc, color=~CATAG6, type="bar") %>% layout(barmode="stack"))
+      } else if(input$demo == "IRSEX") {
+        data <- group_by(slider.data, MJAGE, IRSEX) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
+        return(plot_ly(data, x=~MJAGE, y=~perc, color=~IRSEX, type="bar") %>% layout(barmode="stack"))
+      } else {
+        data <- group_by(slider.data, MJAGE, NEWRACE2) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
+        return(plot_ly(data, x=~MJAGE, y=~perc, color=~NEWRACE2, type="bar") %>% layout(barmode="stack"))
+      }
+    } else if(input$question == "MJREC") {
+      if(input$demo == "CATAG6") {
+        data <- group_by(users.only, MJREC, CATAG6) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
+        return(plot_ly(data, x=~perc, y=~MJREC, color=~CATAG6, type="bar", orientation = 'h') %>% layout(barmode="stack"))
+      } else if(input$demo == "IRSEX") {
+        data <- group_by(users.only, MJREC, IRSEX) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
+        return(plot_ly(data, x=~perc, y=~MJREC, color=~IRSEX, type="bar", orientation = 'h') %>% layout(barmode="stack"))
+      } else {
+        data <- group_by(users.only, MJREC, NEWRACE2) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
+        return(plot_ly(data, x=~perc, y=~MJREC, color=~NEWRACE2, type="bar", orientation = 'h') %>% layout(barmode="stack"))
+      }
+    } else {
+      if(input$demo == "CATAG6") {
+        data <- group_by(users.only, MJDAY30A, CATAG6) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
+        return(plot_ly(data, x=~MJDAY30A, y=~perc, color=~CATAG6, type="bar") %>% layout(barmode="stack"))
+      } else if(input$demo == "IRSEX") {
+        data <- group_by(users.only, MJDAY30A, IRSEX) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
+        return(plot_ly(data, x=~MJDAY30A, y=~perc, color=~IRSEX, type="bar") %>% layout(barmode="stack"))
+      } else {
+        data <- group_by(users.only, MJDAY30A, NEWRACE2) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
+        return(plot_ly(data, x=~MJDAY30A, y=~perc, color=~NEWRACE2, type="bar") %>% layout(barmode="stack"))
+      }
     }
   })
 
  output$demo_plot <- renderPlotly({
   
-   ggplotly(plot(), originalData = FALSE) %>% mutate(ydiff = ymax - ymin) %>% 
-     add_text(
-       x = ~x, y = ~1 - (ymin + ymax) / 2,
-       text = ~ifelse(ydiff > 0.02, round(ydiff, 2), ""),
-       showlegend = FALSE, hoverinfo = "none",
-       color = I("black"), size = I(9)
-     )
-
+   plot()
+   
   })
 })
