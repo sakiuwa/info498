23550d58ff2f1c26f8d002d307b6ce5c5e78539b
Landon Young <young518@uw.edu>
2017-03-07 13:23:59 -0800

    Finished tab, description in UI file

--- Tab 2 (Landon)/server.r
+++ Tab 2 (Landon)/server.r
@@ -11,71 +11,77 @@ library(dplyr)
 library(shiny)
 library(survey)
 
-# Read in marijuana and demographics data from survey
+# Data Prep:
+# mjdata <- read.csv("../data/mjdata.csv")
+# demographics <- read.csv("../data/demo.csv")
+# weights <- read.csv("../data/weights.csv")
+# demo.weights <- left_join(demographics, weights, by = "CASEID")
+# 
+# demo <- select(demo.weights, CASEID, NEWRACE2, IRSEX, CATAG6, ANALWT_C, VESTR)
+# demo$NEWRACE2 <- factor(demo$NEWRACE2, 
+#                         levels=c(1:7),
+#                         labels=c("(1) White", "(2) Black", "(3) Native Am", "(4) Pac. Isl.", "(5) Asian", "(6) NonHisp >1 Race", "(7) Hispanic"))
+# demo$CATAG6 <- factor(demo$CATAG6,
+#                       levels= c(1:6),
+#                       labels=c("(1) 12-17", "(2) 18-25", "(3) 26-34", "(4) 35-49", "(5) 50-64", "(6) 65+"))
+# 
+# users.only <- filter(mjdata, MJEVER == "(1) Yes") %>% select(CASEID, MJAGE, MJREC, MJDAY30A)
+# users.only <- left_join(users.only, demo, by = "CASEID")
 
-mjdata <- read.csv("../data/mjdata.csv")
-demographics <- read.csv("../data/demo.csv")
-weights <- read.csv("../data/weights.csv")
-demo.weights <- left_join(demographics, weights, by = "CASEID")
-
-# Filter users of marijuana only, and select relevant columns
-demo <- select(demo.weights, CASEID, NEWRACE2, IRSEX, CATAG6, ANALWT_C, VESTR)
-demo$NEWRACE2 <- factor(demo$NEWRACE2, 
-                        levels=c(1:7),
-                        labels=c("(1) White", "(2) Black", "(3) Native Am", "(4) Pac. Isl.", "(5) Asian", "(6) NonHisp >1 Race", "(7) Hispanic"))
-demo$CATAG6 <- factor(demo$CATAG6,
-                      levels= c(1:6),
-                      labels=c("(1) 12-17", "(2) 18-25", "(3) 26-34", "(4) 35-49", "(5) 50-64", "(6) 65+"))
-
-users.only <- filter(mjdata, MJEVER == "(1) Yes") %>% select(CASEID, MJAGE, MJREC, MJDAY30A)
-users.only <- left_join(users.only, demo, by = "CASEID")
+# Read in data necessary for this tab
+users.only <- read.csv("../data/tab2-data.csv")
 
 shinyServer(function(input, output) {
-  # Reactive function to choose particular graph depending on the selected question.
+  # Reactive function to choose particular graph depending on the selected question and demographic.
   plot <- reactive({
-    if(input$question == "MJAGE") {
+    if(input$question == "MJAGE") { # If "Age first used?" selected
+      
+      # Deals with the slider input and adjusts data accordingly
       slidevec <- input$agerange
       slidemin <- slidevec[1]
       slidemax <- slidevec[2]
       slider.data <- filter(users.only, MJAGE >= slidemin, MJAGE <= slidemax)
-      if(input$demo == "CATAG6") {
+      
+      if(input$demo == "CATAG6") { # If Age demographic is selected
         data <- group_by(slider.data, MJAGE, CATAG6) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
-        return(plot_ly(data, x=~MJAGE, y=~perc, color=~CATAG6, type="bar") %>% layout(barmode="stack"))
-      } else if(input$demo == "IRSEX") {
+        return(plot_ly(data, x=~MJAGE, y=~count, color=~CATAG6, type="bar") %>% layout(barmode='stack', xaxis=list(title="Age first used marijuana"), yaxis= list(title="# of respondents")))
+        
+      } else if(input$demo == "IRSEX") { # If Sex demographic is selected
         data <- group_by(slider.data, MJAGE, IRSEX) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
-        return(plot_ly(data, x=~MJAGE, y=~perc, color=~IRSEX, type="bar") %>% layout(barmode="stack"))
-      } else {
+        return(plot_ly(data, x=~MJAGE, y=~count, color=~IRSEX, type="bar") %>% layout(barmode="stack", xaxis=list(title="Age first used marijuana"), yaxis= list(title="# of respondents")))
+        
+      } else { # If Race demographic is selected
         data <- group_by(slider.data, MJAGE, NEWRACE2) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
-        return(plot_ly(data, x=~MJAGE, y=~perc, color=~NEWRACE2, type="bar") %>% layout(barmode="stack"))
+        return(plot_ly(data, x=~MJAGE, y=~count, color=~NEWRACE2, type="bar") %>% layout(barmode="stack", xaxis=list(title="Age first used marijuana"), yaxis= list(title="# of respondents")))
       }
-    } else if(input$question == "MJREC") {
+    } else if(input$question == "MJREC") { # If "How long since used?" selected
       if(input$demo == "CATAG6") {
-        data <- group_by(users.only, MJREC, CATAG6) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
-        return(plot_ly(data, x=~perc, y=~MJREC, color=~CATAG6, type="bar", orientation = 'h') %>% layout(barmode="stack"))
+        data <- group_by(users.only, MJREC, CATAG6) %>% summarise(natcount = sum(ANALWT_C)) %>% mutate(natperc = round(natcount/sum(natcount)*100, 2))
+        return(plot_ly(data, x=~MJREC, y=~natperc, color=~CATAG6, type="bar") %>% layout(barmode="stack", xaxis=list(title="Time since last used marijuana"), yaxis=list(title="National %")))
       } else if(input$demo == "IRSEX") {
-        data <- group_by(users.only, MJREC, IRSEX) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
-        return(plot_ly(data, x=~perc, y=~MJREC, color=~IRSEX, type="bar", orientation = 'h') %>% layout(barmode="stack"))
+        data <- group_by(users.only, MJREC, IRSEX) %>% summarise(natcount = sum(ANALWT_C)) %>% mutate(natperc = round(natcount/sum(natcount)*100, 2))
+        return(plot_ly(data, x=~MJREC, y=~natperc, color=~IRSEX, type="bar") %>% layout(barmode="stack", xaxis=list(title="Time since last used marijuana"), yaxis=list(title="National %")))
       } else {
-        data <- group_by(users.only, MJREC, NEWRACE2) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
-        return(plot_ly(data, x=~perc, y=~MJREC, color=~NEWRACE2, type="bar", orientation = 'h') %>% layout(barmode="stack"))
+        data <- group_by(users.only, MJREC, NEWRACE2) %>% summarise(natcount = sum(ANALWT_C)) %>% mutate(natperc = round(natcount/sum(natcount)*100, 2))
+        return(plot_ly(data, x=~MJREC, y=~natperc, color=~NEWRACE2, type="bar") %>% layout(barmode="stack", xaxis=list(title="Time since last used marijuana"), yaxis=list(title="National %")))
       }
-    } else {
+    } else { # If "Last 30 days?" selected
       if(input$demo == "CATAG6") {
-        data <- group_by(users.only, MJDAY30A, CATAG6) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
-        return(plot_ly(data, x=~MJDAY30A, y=~perc, color=~CATAG6, type="bar") %>% layout(barmode="stack"))
+        data <- group_by(users.only, MJDAY30A, CATAG6) %>% summarise(natcount = round(sum(ANALWT_C), 0))
+        return(plot_ly(data, x=~MJDAY30A, y=~natcount, color=~CATAG6, type="bar", hoverinfo='text', text= ~paste('Count: ', format(natcount, big.mark=",", scientific=FALSE))) %>% layout(barmode="stack", xaxis=list(title="Days used in last 30 days"), yaxis=list(title="National count, based on weights")))
       } else if(input$demo == "IRSEX") {
-        data <- group_by(users.only, MJDAY30A, IRSEX) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
-        return(plot_ly(data, x=~MJDAY30A, y=~perc, color=~IRSEX, type="bar") %>% layout(barmode="stack"))
+        data <- group_by(users.only, MJDAY30A, IRSEX) %>% summarise(natcount = round(sum(ANALWT_C), 0)) 
+        return(plot_ly(data, x=~MJDAY30A, y=~natcount, color=~IRSEX, type="bar", hoverinfo='text', text= ~paste('Count: ', format(natcount, big.mark=",", scientific=FALSE))) %>% layout(barmode="stack", xaxis=list(title="Days used in last 30 days"), yaxis=list(title="National count, based on weights")))
       } else {
-        data <- group_by(users.only, MJDAY30A, NEWRACE2) %>% summarise(count = n()) %>% mutate(perc = round(count/sum(count)*100, 2))
-        return(plot_ly(data, x=~MJDAY30A, y=~perc, color=~NEWRACE2, type="bar") %>% layout(barmode="stack"))
+        data <- group_by(users.only, MJDAY30A, NEWRACE2) %>% summarise(natcount = round(sum(ANALWT_C), 0)) 
+        return(plot_ly(data, x=~MJDAY30A, y=~natcount, color=~NEWRACE2, type="bar", hoverinfo='text', text= ~paste('Count: ', format(natcount, big.mark=",", scientific=FALSE))) %>% layout(barmode="stack", xaxis=list(title="Days used in last 30 days"), yaxis=list(title="National count, based on weights")))
       }
     }
   })
 
- output$demo_plot <- renderPlotly({
-  
-   plot()
+  output$demo_plot <- renderPlotly({
+    # Calls reactive function above to output the correct plot
+    plot()
    
   })
 })
--- Tab 2 (Landon)/ui.R
+++ Tab 2 (Landon)/ui.R
@@ -4,23 +4,43 @@
 ### This tab of the shiny app will be looking at correlations between 
 ### marijuana users' self-reported mental health and general health 
 
+### Description
+### This tab aims to show how marijuana users breakdown across age groups, sex, and race. We chose to focus
+### on three questions given in the 2014 National Survey on Drug Use and Health: 
+### 1. How old were you the first time you used marijuana or hashish?
+### 2. How long has it been since you last used marijuana or hashish?
+### and 3. During the past 30 days, on how many days did you use marijuana or hashish?
+### For the first question, we show the number of respondents that reported each age, across demographics.
+### For the second question, we show the national percentage (estimated using the survey weights for each respondent,
+### given in the survey data) broken down across demographics for each response.
+### For the third question, we show the national count (estimated using the survey weights for each respondent, given in the survey data)
+### for each response, broken down across demographics.
+
 fluidPage(
+  # Title of the page
   titlePanel("Demographics of Marijuana Users"),
+  
+  # Sidebar 
   sidebarLayout(
     sidebarPanel(
+      
+      # Input for user to select question of interest.
       selectInput("question", label = h3("Questions for Users"), 
                   choices = c("Age first used?" = 'MJAGE', "How long since used?" = 'MJREC', "Last 30 days?" = 'MJDAY30A'), 
                   selected = 'MJAGE'),
       
+      # Input for user to choose demographic of interest
       radioButtons("demo", label = h3("Demographic of Interest"),
                    choices = c("Age" = 'CATAG6', "Sex" = 'IRSEX', "Race" = 'NEWRACE2'), 
                    selected = 'CATAG6'),
       
+      # Input for user to select an age range; only applies to MJAGE question
       conditionalPanel(condition = "input.question == 'MJAGE'",
         sliderInput("agerange", label = h3("Age Range"), min = 0, max = 80, value = c(0, 30))
       )
     ),
   
+    # Main panel where plot is shown
     mainPanel(
     plotlyOutput(outputId = "demo_plot")
     )
